52a970fbf4cf637442f741eb7a6575e9c02550d1
Craig MacFarlane <camacfarl@gmail.com>
2022-10-29 10:55:36 -0700

    I changed some text as an example

--- app/src/main/java/org/firstinspires/myapplication/MainActivity.java
+++ app/src/main/java/org/firstinspires/myapplication/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "I replaced this with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
